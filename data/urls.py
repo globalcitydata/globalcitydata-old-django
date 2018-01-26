f9ec98ad45a7571b6e740b47f8e7077e30cc4db3
Rafi Barash <rafabarash@gmail.com>
2018-01-26 12:01:53 -0600

    added basic template structure to data

--- data/urls.py
+++ data/urls.py
@@ -13,12 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('^$', TemplateView.as_view(template_name='data/dataset/home.html')),
-    # path('^data/', include('data.urls')),
 ]
