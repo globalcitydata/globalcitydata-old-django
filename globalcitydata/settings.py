4fed98bbec505d08a52def58afaafce891b1135a
Rafi Barash <rafabarash@gmail.com>
2018-01-26 11:07:07 -0600

    added runtime.txt

--- globalcitydata/settings.py
+++ globalcitydata/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
