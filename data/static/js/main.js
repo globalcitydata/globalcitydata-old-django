422f67ebaaf2ef2b6fda67126fd21b4f0fef1a93
Rafi Barash <rafabarash@gmail.com>
2018-02-14 11:36:19 -0600

    made detail page a lil prettier

--- data/static/js/main.js
+++ data/static/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
   $('.lazy').lazy();
 });
+
+$(".btn").click(function() {
+  var lable = $(".btn").text().trim();
+  var hidden = $(".hiddenText").text().trim();
+  if (lable == "Request dataset access") {
+    $(".btn").text(hidden)
+  } else {
+    $(".btn").text("Request dataset access")
+  }
+});
