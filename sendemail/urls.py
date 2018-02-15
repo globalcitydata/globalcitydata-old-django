c4d31ed83417b847891b9a9f2a53a0d3aa34dd5b
Rafi Barash <rafabarash@gmail.com>
2018-02-14 22:52:18 -0600

    added dataset submit form

--- sendemail/urls.py
+++ sendemail/urls.py
@@ -21,6 +21,6 @@ app_name = 'sendemail'
 
 urlpatterns = [
     path('success/', views.successView, name='success'),
-    path('submit-dataset/', views.DatasetSubmitForm, name='submitDataset'),
+    path('submit-dataset/', views.submitDatasetView, name='submitDataset'),
     path('contact/', views.contactView, name='email'),
 ]
